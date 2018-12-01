cdd8feff587e5145eee18e0d9cb0f93381d75915
Jake Howard <git@theorangeone.net>
2018-12-01 14:24:43 +0000

    Add day 1 part 2 solution in Python

--- day-1-chronal-calibration/day-1.py
+++ day-1-chronal-calibration/day-1.py
@@ -1,4 +1,15 @@
 with open('data.txt', 'r') as f:
-    data = [int(line.strip()) for line in f.readlines()]
+    frequencies = [int(line.strip()) for line in f.readlines()]
 
-print(sum(data))
+print(sum(frequencies))
+
+current_frequency = 0
+seen_frequencies = [current_frequency]
+for i in range(9999):
+    for frequency in frequencies:
+        current_frequency += frequency
+        if current_frequency in seen_frequencies:
+            print(current_frequency)
+            exit()
+        seen_frequencies.append(current_frequency)
+    print('pass {} {}'.format(i, current_frequency))
