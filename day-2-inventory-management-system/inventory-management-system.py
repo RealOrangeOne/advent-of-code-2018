9e0d4ffa2e0cf2013d049fb6e5f8a578ef3e9460
Jake Howard <git@theorangeone.net>
2018-12-02 12:17:17 +0000

    Modify day 2 part 1 to require _much_ less code

--- day-2-inventory-management-system/inventory-management-system.py
+++ day-2-inventory-management-system/inventory-management-system.py
@@ -3,17 +3,12 @@ from collections import Counter
 with open('data.txt', 'r') as f:
     ids = [id.strip() for id in f.readlines()]
 
-accumulator = {}
+accumulator = []
 
 for id in ids:
-    id_counter = Counter(list(id))
-    counts = set(id_counter.values())
-    for letter_count in counts:
-        if letter_count not in accumulator:
-            accumulator[letter_count] = 0
-        accumulator[letter_count] += 1
+    accumulator.extend(set(Counter(id).values()))
 
-print(accumulator[2] * accumulator[3])
+print(accumulator.count(2) * accumulator.count(3))
 
 seen = set()
 
