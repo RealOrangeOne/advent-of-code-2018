e4b3192cbd16f634ad671f19f4334b83ef5ae8fb
Jake Howard <git@theorangeone.net>
2018-12-03 13:23:03 +0000

    Add solution for day 3 part 1 in Python

---
+++ day-3-no-matter-how-you-slice-it/no-matter-how-you-slice-it.py
@@ -0,0 +1,32 @@
+from collections import namedtuple, Counter
+import re
+
+CLAIM_RE = re.compile("#(\d+?) @ (\d+?),(\d+?): (\d+?)x(\d+)")
+
+_Claim = namedtuple('Claim', ['id', 'start_x', 'start_y', 'width', 'height'])
+
+class Claim(_Claim):
+    def internal_coordinates(self):
+        coordinates = []
+        for x in range(self.start_x, self.start_x + self.width):
+            for y in range(self.start_y, self.start_y + self.height):
+                coordinates.append((x, y))
+        return coordinates
+
+
+with open('data.txt', 'r') as f:
+    claims = [
+        Claim(*[int(num) for num in CLAIM_RE.match(line).groups()])
+        for line in f.readlines()
+    ]
+
+claim_patches = []
+
+for claim in claims:
+    claim_patches.extend(claim.internal_coordinates())
+
+claim_patch_counter = Counter(claim_patches)
+
+claim_patch_counter_values = list(claim_patch_counter.values()).count(1)
+
+print(len(claim_patch_counter) - claim_patch_counter_values)
